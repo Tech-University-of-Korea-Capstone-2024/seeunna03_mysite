eaf544de8c74ab855a87d07e297889f4c1c654f7
seeunna03 <seeunna03@gmail.com>
2025-03-20 18:01:34 +0900

    schedule.csv 하드코딩>> csv 받아오는 코드로 수정 v1

--- match/views.py
+++ match/views.py
@@ -1,9 +1,24 @@
+import csv
+import os
 from django.shortcuts import render
-from .models import Schedule
+from django.conf import settings
 
 def schedule_list(request):
-    schedules = Schedule.objects.all().order_by('month', 'day', 'time')
-    context = {
-        'schedules': schedules
-    }
-    return render(request, 'match/schedule_list.html', context)
+    schedule_path = os.path.join(settings.BASE_DIR, "baseball_data", "schedule.csv")  # CSV 파일 경로
+    schedules = []
+
+    with open(schedule_path, newline="", encoding="utf-8") as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)
+        for row in reader:
+            schedules.append({
+                "month": row[0],
+                "day": row[1],
+                "weekday": row[2],
+                "time": row[3],
+                "stadium": row[4],
+                "team1": row[5],
+                "team2": row[6],
+            })
+
+    return render(request, "match/schedule_list.html", {"schedules": schedules})
